80aa3bb98a3157a79164d204fb58b2bcb0318571
John Allen <johnallen@Johns-MacBook-Pro-2.local>
2024-04-21 21:22:57 +0100

    chore: migrated from raw generator to natural generator

--- packages/animal/src/animal.generator.ts
+++ packages/animal/src/animal.generator.ts
@@ -1,15 +1,24 @@
-import { Generator } from "@chancejs/generator";
+import { Generator, GeneratorOptions } from "@chancejs/generator";
 import { AnimalOptionTypeException } from "./exceptions";
+import { NaturalGenerator } from "@chancejs/natural";
 import { AnimalOptions,  AnimalTypes,  IAnimalGenerator } from "./interfaces";
 import { animals } from "./constants";
 
 export class AnimalGenerator extends Generator implements IAnimalGenerator {
+  private naturalGenerator: NaturalGenerator;
+constructor(options: GeneratorOptions) {
+    super(options);
+    this.naturalGenerator = new NaturalGenerator(options);
+  }
+
   animal(options?: AnimalOptions): string {
 
     if (options?.type && !Object.values(AnimalTypes).includes(options?.type))
       throw new AnimalOptionTypeException();
+
+   
     
-    const type =  options?.type ?? Object.keys(animals)[this.generator()*Object.keys(animals).length - 1 << 0] as AnimalTypes
-    return animals[type][this.generator()*animals[type].length << 0] 
+    const type =  options?.type ?? Object.keys(animals)[ this.naturalGenerator.natural({max:Object.keys(animals).length - 1})] as AnimalTypes
+    return animals[type][this.naturalGenerator.natural({max:animals[type].length -1})] 
   }
 }
