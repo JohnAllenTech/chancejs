e0d7c62e0f9a3b56527fecd6bf68a43eb6ff6938
John <46611809+JohnAllenTech@users.noreply.github.com>
2024-04-30 21:11:15 +0100

    chore: fixed flaky test (#15)

--- packages/person/src/age/age.test.ts
+++ packages/person/src/age/age.test.ts
@@ -5,34 +5,34 @@ describe('Testing Person Age', () => {
   it('returns an random age that is within the default range', () => {
     const a = age()
     expect(typeof a).toEqual('number')
-    expect(a).toBeGreaterThan(ageRanges.ADULT_AGE_MIN)
-    expect(a).toBeLessThan(ageRanges.ADULT_AGE_MAX)
+    expect(a).toBeGreaterThanOrEqual(ageRanges.ADULT_AGE_MIN)
+    expect(a).toBeLessThanOrEqual(ageRanges.ADULT_AGE_MAX)
   })
   it('returns an random age that is within the child range', () => {
     const a = age({ type: 'child' })
     expect(typeof a).toEqual('number')
-    expect(a).toBeGreaterThan(ageRanges.CHILD_AGE_MIN)
-    expect(a).toBeLessThan(ageRanges.CHILD_AGE_MAX)
+    expect(a).toBeGreaterThanOrEqual(ageRanges.CHILD_AGE_MIN)
+    expect(a).toBeLessThanOrEqual(ageRanges.CHILD_AGE_MAX)
   })
 
   it('returns an random age that is within the adult range', () => {
     const a = age({ type: 'adult' })
     expect(typeof a).toEqual('number')
-    expect(a).toBeGreaterThan(ageRanges.ADULT_AGE_MIN)
-    expect(a).toBeLessThan(ageRanges.ADULT_AGE_MAX)
+    expect(a).toBeGreaterThanOrEqual(ageRanges.ADULT_AGE_MIN)
+    expect(a).toBeLessThanOrEqual(ageRanges.ADULT_AGE_MAX)
   })
 
   it('returns an random age that is within the senior range', () => {
     const a = age({ type: 'senior' })
     expect(typeof a).toEqual('number')
-    expect(a).toBeGreaterThan(ageRanges.SENIOR_AGE_MIN)
-    expect(a).toBeLessThan(ageRanges.SENIOR_AGE_MAX)
+    expect(a).toBeGreaterThanOrEqual(ageRanges.SENIOR_AGE_MIN)
+    expect(a).toBeLessThanOrEqual(ageRanges.SENIOR_AGE_MAX)
   })
 
   it('returns an random age that is within the total range', () => {
     const a = age({ type: 'all' })
     expect(typeof a).toEqual('number')
-    expect(a).toBeGreaterThan(ageRanges.AGE_MIN)
-    expect(a).toBeLessThan(ageRanges.AGE_MAX)
+    expect(a).toBeGreaterThanOrEqual(ageRanges.AGE_MIN)
+    expect(a).toBeLessThanOrEqual(ageRanges.AGE_MAX)
   })
 })
