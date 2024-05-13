d3b3c2c93881d205d5d77572e7c5f5f7ff8e4ecc
John <46611809+JohnAllenTech@users.noreply.github.com>
2024-05-13 23:25:29 +0100

    Update packages/location/src/phone/phone.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- packages/location/src/phone/phone.ts
+++ packages/location/src/phone/phone.ts
@@ -1,6 +1,5 @@
 import { Location } from '../generator'
-import { PhoneGeneratorFunction } from './interfaces'
-import { PhoneOptions } from './interfaces'
+import { PhoneGeneratorFunction, PhoneOptions } from './interfaces'
 
 export const phone: PhoneGeneratorFunction = (options?: PhoneOptions) => {
   const location = new Location({ seed: options?.seed })
