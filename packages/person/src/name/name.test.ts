301e392fe1b357bea4a78576fb5c9bfa54f879a9
John <46611809+JohnAllenTech@users.noreply.github.com>
2024-05-14 10:12:02 +0100

    chore: fixed flaky tests (#53)

--- packages/person/src/name/name.test.ts
+++ packages/person/src/name/name.test.ts
@@ -1,41 +1,55 @@
+import { times } from '@chancejs/generator'
 import { name } from './name'
 
-describe('Testing Name Name function', () => {
+describe('Testing person name function', () => {
   it('returns an random full name', () => {
-    const result = name()
-    expect(typeof result).toEqual('string')
-    expect(result.length).toBeGreaterThanOrEqual(2)
-    expect(result.length).toBeLessThanOrEqual(30)
-    expect(result.split(' ').length).toEqual(2)
-    expect(/[a-zA-Z]+\ [a-zA-Z]+/.test(result)).toEqual(true)
+    times(1000, () => {
+      const result = name()
+      expect(typeof result).toEqual('string')
+      expect(result.length).toBeGreaterThanOrEqual(2)
+      expect(result.length).toBeLessThanOrEqual(30)
+      expect(result.split(' ').length).toBeLessThanOrEqual(4)
+      expect(result.split(' ').length).toBeGreaterThanOrEqual(2)
+      expect(/[\p{L}\s]+/.test(result)).toEqual(true)
+    })
   })
 
   it('returns an random full name with middle name', () => {
-    const result = name({ middle: true })
-    expect(typeof result).toEqual('string')
-    expect(result.length).toBeGreaterThanOrEqual(3)
-    expect(/[a-zA-Z]+\ [a-zA-Z]+\ [a-zA-Z]+/.test(result)).toEqual(true)
+    times(1000, () => {
+      const result = name({ middle: true })
+      expect(typeof result).toEqual('string')
+      expect(result.length).toBeGreaterThanOrEqual(3)
+      expect(/[\p{L}\s]+/.test(result)).toEqual(true)
+    })
   })
 
   it('returns an random full name with middle initial', () => {
-    const result = name({ middle_initial: true })
-    expect(typeof result).toEqual('string')
-    expect(result.length).toBeGreaterThanOrEqual(3)
-    expect(/[a-zA-Z]+\ [a-zA-Z]\.\ [a-zA-Z]+/.test(result)).toEqual(true)
-    expect(result.split(' ')[1].length).toEqual(2)
+    times(1000, () => {
+      const result = name({ middle_initial: true })
+      expect(typeof result).toEqual('string')
+      expect(result.length).toBeGreaterThanOrEqual(3)
+      expect(
+        /[\p{L}\p{M}']+ [\p{L}\p{M}]\. [\p{L}\p{M}']+/u.test(result)
+      ).toEqual(true)
+      expect(result.split(' ')[1].length).toEqual(2)
+    })
   })
 
   it('returns an random full name with prefix', () => {
-    const result = name({ prefix: true })
-    expect(typeof result).toEqual('string')
-    expect(result.length).toBeGreaterThanOrEqual(3)
-    expect(/[a-zA-Z]{2,4}\.? [a-zA-Z]+\ [a-zA-Z]+/.test(result)).toEqual(true)
+    times(1000, () => {
+      const result = name({ prefix: true })
+      expect(typeof result).toEqual('string')
+      expect(result.length).toBeGreaterThanOrEqual(3)
+      expect(/[a-zA-Z]{2,4}\.? [a-zA-Z]+\ [a-zA-Z]+/.test(result)).toEqual(true)
+    })
   })
 
   it('returns an random full name with suffix', () => {
-    const result = name({ suffix: true })
-    expect(typeof result).toEqual('string')
-    expect(result.length).toBeGreaterThanOrEqual(3)
-    expect(/[a-zA-Z]+\ [a-zA-Z]+\ [a-zA-Z\.]+/.test(result)).toEqual(true)
+    times(1000, () => {
+      const result = name({ suffix: true })
+      expect(typeof result).toEqual('string')
+      expect(result.length).toBeGreaterThanOrEqual(3)
+      expect(/[\p{L}\s\.]+/.test(result)).toEqual(true)
+    })
   })
 })
