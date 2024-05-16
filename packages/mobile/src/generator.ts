b217e6df8dbbd1750e840d47eac14b65fb79b995
John Allen <46611809+JohnAllenTech@users.noreply.github.com>
2024-05-16 22:52:47 +0100

    chore: added android id function to mobile package

--- packages/mobile/src/generator.ts
+++ packages/mobile/src/generator.ts
@@ -1,17 +1,27 @@
 import { Generator, GeneratorOptions } from '@chancejs/generator'
 import { IMobile, MobileOptions } from './interfaces'
 import { NaturalGenerator } from '@chancejs/natural'
+import { StringGenerator } from '@chancejs/string'
+import { AndroidIdOptions } from './android_id'
 
 export class Mobile extends Generator implements IMobile {
   private naturalGenerator: NaturalGenerator
+  private string: StringGenerator
 
   constructor(options: GeneratorOptions) {
     super(options)
     this.naturalGenerator = new NaturalGenerator(options)
+    this.string = new StringGenerator(options)
   }
 
-  public android_id(options?: MobileOptions): string {
-    return 'string'
+  public android_id(_options?: AndroidIdOptions): string {
+    return (
+      'APA91' +
+      this.string.string({
+        pool: '0123456789abcefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_',
+        length: 178,
+      })
+    )
   }
 
   public apple_token(options?: MobileOptions): string {
