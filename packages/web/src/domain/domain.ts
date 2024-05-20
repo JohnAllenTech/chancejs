e43b0ac5271630335688c8a7a85d36c9b29087e0
John Allen <46611809+JohnAllenTech@users.noreply.github.com>
2024-05-20 22:30:39 +0100

    chore: Added todo

--- packages/web/src/domain/domain.ts
+++ packages/web/src/domain/domain.ts
@@ -2,6 +2,7 @@ import { Web } from '../generator'
 import { DomainGeneratorFunction } from './interfaces'
 import { DomainOptions } from './interfaces'
 
+// #TODO fix the typing on this when it is imported
 export const domain: DomainGeneratorFunction = (options?: DomainOptions) => {
   const web = new Web({ seed: options?.seed })
   return web.domain(options)
