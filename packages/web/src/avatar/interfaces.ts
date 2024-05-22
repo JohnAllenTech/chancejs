e7747084337322a52f2594f8af80a78cbff5768f
John Allen <46611809+JohnAllenTech@users.noreply.github.com>
2024-05-22 23:22:17 +0100

    chore: removing unused enums

--- packages/web/src/avatar/interfaces.ts
+++ packages/web/src/avatar/interfaces.ts
@@ -1,31 +1,5 @@
 import { WebOptions } from '../interfaces'
 
-enum PROTOCOLS {
-  http = 'http',
-  https = 'https',
-}
-enum FILE_TYPES {
-  bmp = 'bmp',
-  gif = 'gif',
-  jpg = 'jpg',
-  png = 'png',
-}
-// enum FALLBACKS {
-//   '404' = '404', // Return 404 if not found
-//   mm = 'mm', // Mystery man
-//   identicon = 'identicon', // Geometric pattern based on hash
-//   monsterid = 'monsterid', // A generated monster icon
-//   wavatar = 'wavatar', // A generated face
-//   retro = 'retro', // 8-bit icon
-//   blank = 'blank', // A transparent png
-// }
-enum RATINGS {
-  g = 'g',
-  pg = 'pg',
-  r = 'r',
-  x = 'x',
-}
-
 export interface AvatarOptions extends WebOptions {
   protocol?: 'http' | 'https'
   fileExtension?: 'bmp' | 'gif' | 'jpg' | 'png'
